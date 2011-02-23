93fecd520d8355980171b2fae884f2923c1f731f
Bart Mosley <bartmosley@Bart-Mosleys-Mac-mini.local>
2011-02-23 11:08:36 -0500

    portalloc

--- portalloc/test.py
+++ portalloc/test.py
@@ -20,3 +20,10 @@ spx_x = [returns[dt]['SPXExcess'] for dt in returns.refcolumn
           if returns[dt]['SPXExcess']]
 leh_x = [returns[dt]['LEHExcess'] for dt in returns.refcolumn
           if returns[dt]['LEHExcess']]
+
+
+X = np.vstack((spx_tr, leh_tr))
+C = np.cov(X)
+
+m = np.array((np.average(spx_tr), np.average(leh_tr)))
+
