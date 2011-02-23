1fd44554fa3d865e969a6216db692d1a498a782e
bart mosley <bartm@bondgeek.com>
2011-02-23 09:13:20 -0500

    linear regression

--- linreg/example.py
+++ linreg/example.py
@@ -22,8 +22,10 @@ yr = polyval((mr, br), t)
 # standard error
 err = sqrt( sum((yr - yn)**2/(n-1)) )
 
-#output
+# compare with scipy.stats.linregress
+(m_s, b_s, r_sqrd, p_val, stderr) = stats.linregress(t, yn)
 
+#output
 plt.title("linear regression example")
 plt.plot(t, y, 'g.--')
 plt.plot(t, yn, 'k.')
@@ -31,3 +33,11 @@ plt.plot(t, yr, 'r.-')
 plt.legend(('original', 'plus noise', 'regress'))
 
 plt.show()
+
+print("plt.close() to close graph")
+print("\npolyfit regression")
+print("\n m %s b %s stderr %s" % (mr, br, err))
+print("\nstats.linregress")
+print("\n m %s b %s stderr %s r^2 %s pval %s" % 
+      (m_s, b_s, stderr, r_sqrd, p_val))
+
