b0d66461b325a0a99bcdfc6adbcb2bf13a91203f
Bart Mosley <bartmosley@Bart-Mosleys-Mac-mini.local>
2011-03-29 16:12:36 -0400

    hist_cov v2

---
+++ optimize/hist_cov_v2.py
@@ -0,0 +1,49 @@
+from bgpy.xldb import XLdb
+from alprion import DROPBOX, PathJoin, DATADIR
+
+from bgpy.math.garch import GARCH
+
+import numpy as np
+import scipy as sp
+import scipy.stats as ss
+import scipy.linalg as sl
+
+studybook = PathJoin(DROPBOX, 
+                     "MarketData/Benchmarks", 
+                     "mlindex_study_v2.xls")
+
+if 'studydata' not in vars():
+    studydata = XLdb(studybook, 
+                     sheet_name="Summary", 
+                     startrow=9, 
+                     idx_column=1)
+
+rkeys = ['TRCoreLong', 
+          'TRCoreLong Int', 
+          'TRCoreIntermediate',
+          'TRCoreShort Int',
+          'TRCoreShort',
+          'TRRiskLong',
+          'TRRiskLong Int',
+          'HTRCoreLong', 
+          'HTRCoreLong Int', 
+          'HTRCoreIntermediate',
+          'HTRCoreShort Int',
+          'HTRCoreShort',
+          'HTRRiskLong',
+          'HTRRiskLong Int'
+          ]
+
+
+rdata = [[] for k in rkeys]
+covmtx = {}
+for dt in studydata.refcolumn[1:]:
+    
+    for n in range(len(rkeys)):
+        k = rkeys[n]
+        rdata[n].append(studydata[dt][k])        
+
+    if len(rdata[0]) > 30:
+        covmtx[dt] = np.cov(rdata)
+
+
