b1fbe76e978ec1b653b2f7d5ff00140cc73ce0d1
bart mosley <bartm@bondgeek.com>
2011-08-01 08:58:58 -0400

    updating black models

--- optionpricing/VanillaOption.py
+++ optionpricing/VanillaOption.py
@@ -1,13 +1,33 @@
 import numpy as np
 
+from scipy.stats import norm
+
 from random1 import gaussianBoxMuller
     
 class PayOff(object):
+    '''
+    Defines the option.
+    
+    bs_flag class variable indicates if option should be treated as call or put
+    in Black Scholes
+    
+    '''
+    class BSFLAG:
+        call = 1
+        put = 2
+        
+    bs_flag = None
     def __init__(self, strike):
         self._strike = strike
     
     def payoff(self, spot):
-        "override in derived class to define option payoff
+        '''
+        Override in derived class to define option payoff
+        
+        When called this function will return the payoff given the spot price 
+        of the option.
+        
+        '''
         return None
     
     @property
@@ -15,10 +35,14 @@ class PayOff(object):
         return self._strike
 
 class Call(PayOff):
+    bs_flag = BSFLAG.call
+    
     def payoff(self, spot):
         return max(0, spot - self.strike)
 
 class Put(PayOff):
+    bs_flag = BSFLAG.put
+    
     def payoff(self, spot):
         return max(0, self.strike - spot) 
         
@@ -32,9 +56,17 @@ class VanillaOption(object):
         return self._expiry
 
     @property
-    def payoff(self)
+    def payoff(self):
+        # return the payoff object's payoff function
         return self._payoff.payoff
+    
+    @property
+    def strike(self):
+        return self._payoff.strike
 
+    @property
+    def bs_flag(self):
+        return self._payoff.bs_flag        
 
 def SimpleMC1(option,
              spot,
@@ -60,4 +92,37 @@ def SimpleMC1(option,
     
     return mean
     
+
+def SimpleBlackScholes(option, spot, vol, r):
+    '''
+    Black Scholes Formula
     
+    '''
+    
+    T = option.expiry
+    X = option.strike
+    variance = vol * vol
+    
+    d1 = (np.log(spot/X)+(r+variance/2.)*T)/(vol*np.sqrt(T))
+
+    d2 = d1-vol*np.sqrt(T)
+    
+    if option.bs_flag == PayOff.BSFLAG.call:
+        return spot*norm.cdf(d1)-X*np.exp(-r*T)*norm.cdf(d2)
+
+    else:
+        return X*np.exp(-r*T)*norm.cdf(-d2)-spot*norm.cdf(-d1)
+
+def black76(expry, F, X, r, vol):
+    '''
+    Black 76 call price
+    
+    '''
+    d1 = (np.log(F/X)-(0.5*(vol**2)*expry))/(vol*np.sqrt(expry))   
+    pv = np.exp(-r * expry)
+
+    nd1 = norm.cdf(d1)
+
+    c = pv*(F*norm.cdf(d1) - X*norm.cdf(d1 - vol * np.sqrt(expry)))
+
+    return (c, d1, pv, nd1, pv*nd1)
--- optionpricing/black76.py
+++ optionpricing/black76.py
@@ -2,6 +2,10 @@ from scipy.stats import norm
 import numpy as np
 
 def black76(expry, F, X, r, vol):
+    '''
+    Black 76 call price
+    
+    '''
     d1 = (np.log(F/X)-(0.5*(vol**2)*expry))/(vol*np.sqrt(expry))   
     pv = np.exp(-r * expry)
 
@@ -10,3 +14,43 @@ def black76(expry, F, X, r, vol):
     c = pv*(F*norm.cdf(d1) - X*norm.cdf(d1 - vol * np.sqrt(expry)))
 
     return (c, d1, pv, nd1, pv*nd1)
+
+def CND(X):
+    ''' 
+    Cumulative normal distribution
+
+    '''
+
+    a1, a2, a3, a4, a5 = (0.31938153, -0.356563782, 1.781477937, 
+                         -1.821255978, 1.330274429)
+    L = np.abs(X)
+
+    K = 1.0 / (1.0 + 0.2316419 * L)
+
+    w1 = 1.0/np.sqrt(2*pi)*np.exp(-L*L/2.)
+    
+    w2 = (a1*K+a2*K*K+a3*np.pow(K,3) + a4*pow(K,4) + a5*pow(K,5))
+    
+    w = 1.0 - w1 * w2
+    
+    if X<0:
+        w = 1.0-w
+
+    return w
+
+def BlackScholes(CallPutFlag,S,X,T,r,v):
+    '''
+    Black Scholes Formula
+    
+    '''
+    d1 = (log(S/X)+(r+v*v/2.)*T)/(v*sqrt(T))
+
+    d2 = d1-v*sqrt(T)
+    if CallPutFlag=='c':
+
+        return S*CND(d1)-X*exp(-r*T)*CND(d2)
+
+    else:
+        return X*exp(-r*T)*CND(-d2)-S*CND(-d1)
+        
+        
